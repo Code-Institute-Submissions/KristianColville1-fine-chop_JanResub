90fd3c5de9ab690d66e5238b1572e23b8f2284cd
KristianColville1 <kristiancolville1@gmail.com>
2022-10-16 12:03:20 +0100

    Added home app and error handler paths

--- core/urls.py
+++ core/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import handler404, handler500
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', include('home.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+handler404 = handler404
+handler500 = handler500
