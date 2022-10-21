c1230c53f1f2e4278cdfd9319e63d068ae4ecffe
KristianColville1 <kristiancolville1@gmail.com>
2022-10-21 14:00:02 +0100

    Refactor js and close open dropdowns

--- static/js/menu.js
+++ static/js/menu.js
@@ -5,25 +5,33 @@ $(document).ready(function () {
      * Improves the UI and makes the website more cohesive.
      */
 
-    // if the user clicks a dropdown then do this
+    // Gets dropdowns 
+    const dropdowns = document.getElementsByClassName('.dropdown-menu');
+
+    // when called slides up any open dropdowns
+    let slideAllDropdownsUp = () =>{
+      $(".dropdown-menu").each(function(){
+        $(this).slideUp().removeClass("menu-activated");
+      });
+    }
+
+    // if the user mouse enters
     $(".dropdown-toggle").on("mouseenter", function (e) {
         e.preventDefault();
         let menu = $(this).next()
         if($(menu).hasClass("menu-activated")){
-            $(menu).slideUp('slow');
-            $(menu).removeClass("menu-activated");
+            $(menu).slideUp("slow").removeClass("menu-activated");
         } else {
-            $(menu).slideDown('fast');
-            $(menu).addClass("menu-activated");
+            slideAllDropdownsUp(); // before sliding down new menu close others up
+            $(menu).slideDown("fast").addClass("menu-activated");
         }
     });
 
-    // if the mouse leaves the dropdown then do this
+    // if the mouse leaves 
     $(".dropdown-menu").on("mouseleave", function (e) {
         e.preventDefault();
         let menu = $(this)
-        $(menu).slideUp();
-        $(menu).removeClass("menu-activated")
+        $(menu).slideUp().removeClass("menu-activated");
     });
 });
 
