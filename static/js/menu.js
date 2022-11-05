aa3a2c876d2017f58be5e39b90064a75cb9d7058
KristianColville1 <kristiancolville1@gmail.com>
2022-11-05 16:53:02 +0000

    Added nav bg if allauth wrapper class element exists to improve UI/UX

--- static/js/menu.js
+++ static/js/menu.js
@@ -5,21 +5,21 @@ $(document).ready(function () {
      * Improves the UI and makes the website more cohesive.
      */
 
-    // Gets dropdowns 
-    const dropdowns = document.getElementsByClassName('.dropdown-menu');
+    // Gets dropdowns
+    const dropdowns = document.getElementsByClassName(".dropdown-menu");
 
     // when called slides up any open dropdowns
-    let slideAllDropdownsUp = () =>{
-      $(".dropdown-menu").each(function(){
-        $(this).slideUp().removeClass("menu-activated");
-      });
-    }
+    let slideAllDropdownsUp = () => {
+        $(".dropdown-menu").each(function () {
+            $(this).slideUp().removeClass("menu-activated");
+        });
+    };
 
     // if the user mouse enters
     $(".dropdown-toggle").on("mouseenter", function (e) {
         e.preventDefault();
-        let menu = $(this).next()
-        if($(menu).hasClass("menu-activated")){
+        let menu = $(this).next();
+        if ($(menu).hasClass("menu-activated")) {
             $(menu).slideUp("slow").removeClass("menu-activated");
         } else {
             slideAllDropdownsUp(); // before sliding down new menu close others up
@@ -27,20 +27,37 @@ $(document).ready(function () {
         }
     });
 
-    // if the mouse leaves 
+    // if the mouse leaves
     $(".dropdown-menu").on("mouseleave", function (e) {
         e.preventDefault();
-        let menu = $(this)
+        let menu = $(this);
         $(menu).slideUp().removeClass("menu-activated");
     });
+
+    /**
+     * Checks to see if the class allauth-wrapper exists and performs an 
+     * action based on that behaviour for scrolling and adding a background
+     * to the navigation menu.
+     */
+
+    let navWrapper = $(".nav-wrapper");
+    let checkAllauthForm = document.getElementsByClassName("allauth-container");
+    // if it does exist and there is an element with this class then add background
+    if (checkAllauthForm.length != 0) {
+        navWrapper.addClass("nav-bg");
+    }
+    window.addEventListener("scroll", function () {
+        // nested condition if allauth wrapper class elements don't exist
+        if (checkAllauthForm.length === 0) {
+            if (this.window.scrollY > 90) {
+                navWrapper.addClass("nav-bg");
+            }
+            if (this.window.scrollY < 50) {
+                navWrapper.removeClass("nav-bg");
+            }
+        }
+    });
+    
 });
 
-let navWrapper = $('.nav-wrapper');
-window.addEventListener("scroll", function () {
-    if (this.window.scrollY > 90) {
-        navWrapper.addClass('nav-bg');
-    }
-    if (this.window.scrollY < 90) {
-        navWrapper.removeClass('nav-bg');
-    }
-});
+
