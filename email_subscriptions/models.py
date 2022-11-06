f900b0de74793cadf1b2de14b7fd47718199e633
KristianColville1 <kristiancolville1@gmail.com>
2022-11-06 19:50:08 +0000

    Added model for subscribers to newsletter

--- email_subscriptions/models.py
+++ email_subscriptions/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Subscriber(models.Model):
+    """ Subscriber Model """
+    email = models.CharField(max_length=320, unique=True)
+    name = models.CharField(max_length=120, unique=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.email
