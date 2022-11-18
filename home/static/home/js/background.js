d8104a98285e16873ae65d3056d53ecb7be0cffa
KristianColville1 <kristiancolville1@gmail.com>
2022-11-18 16:00:42 +0000

    Added background iteration for images on home page

---
+++ home/static/home/js/background.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    let timeToDisplay = 7000;
+
+    let slideshow = $("#home-bg-slideshow");
+    const urls = [
+        "../static/img/home-page/home-2.webp",
+        "../static/img/home-page/home-3.webp",
+        "../static/img/home-page/home-4.webp",
+        "../static/img/home-page/home-1.webp",
+    ];
+
+    let index = 0;
+    let transition = function () {
+        var url = urls[index];
+        slideshow.css("background", "url(" + url + ") no-repeat center center");
+        slideshow.css("background-size", "cover");
+        index++;
+        if (index > urls.length - 1) {
+            index = 0;
+        }
+    };
+
+    const iterateSlideShow = function () {
+        transition();
+        slideshow.fadeIn("slow", function () {
+            setTimeout(function () {
+                slideshow.fadeOut("fast", iterateSlideShow);
+            }, timeToDisplay);
+        });
+    };
+
+    iterateSlideShow();
+});
