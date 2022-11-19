0048b5a1b196e43b48d8c1be4f85f1cb4a013d51
KristianColville1 <kristiancolville1@gmail.com>
2022-11-19 09:02:33 +0000

    Added abstract mclass model fooditem for menu app

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class FoodItem(models.Model):
+    """Abstract class for MenuItems"""
+
+    name = models.CharField(max_length=250, unique=True)
+    description = models.CharField(max_length=1000, unique=False)
+
+    class Meta:
+        abstract = True
+
+
