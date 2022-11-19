5a4d5e6e3914e8341d179c9e08ef6b4d47ea8628
KristianColville1 <kristiancolville1@gmail.com>
2022-11-19 17:30:08 +0000

    Added set up view for food menus

--- menu/views.py
+++ menu/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def get_menu(request):
     return render(request, 'menu/index.html')
+
+
+def get_food_menu(request, str):
+    return render(request, 'menu/index.html')
