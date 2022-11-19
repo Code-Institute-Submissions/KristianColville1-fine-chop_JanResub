84e6b2a041281fb2cc283edac458c61ba9798ed0
KristianColville1 <kristiancolville1@gmail.com>
2022-11-19 10:00:46 +0000

    Registered menu item class in admin

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import MenuItem
 
-# Register your models here.
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    """
+    MenuItemAdmin provides staff and site owner with ability
+    to manage menu items for creation and updating.
+    """
+    model = MenuItem
+    list_display = [field.name for field in MenuItem._meta.get_fields()]
+    search_fields = [field.name for field in MenuItem._meta.get_fields()]
