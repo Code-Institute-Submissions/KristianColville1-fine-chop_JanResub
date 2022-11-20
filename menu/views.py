f9b9fd38acc0810a7b3829f2838e9ca2822e9fb4
KristianColville1 <kristiancolville1@gmail.com>
2022-11-20 19:01:43 +0000

    Update views for main menu and food menus

--- menu/views.py
+++ menu/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import MenuItem, Category
 
 
 def get_menu(request):
-    return render(request, 'menu/index.html')
+    """
+    Brings the user to the food menu page
+    """
+    categories = Category.objects.all()
+    context = {
+        'main_menu': 'main_menu',
+        'categories': categories,
+    }
+    return render(request, 'menu/index.html', context)
 
 
 def get_food_menu(request, str):
-    return render(request, 'menu/index.html')
+    categories = Category.objects.all()
+    context = {
+        'main_menu': 'main_menu',
+        'categories': categories,
+        'main_menu': False,
+    }
+    return render(request, 'menu/index.html', context)
