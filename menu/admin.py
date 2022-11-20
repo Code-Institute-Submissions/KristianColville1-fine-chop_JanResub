89e6d78be64c87576ced4062edea782da4c676ce
KristianColville1 <kristiancolville1@gmail.com>
2022-11-20 19:11:01 +0000

    Added categories to admin and registered model admin

--- menu/admin.py
+++ menu/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import MenuItem
+from .models import MenuItem, Category
 
 
 @admin.register(MenuItem)
@@ -9,5 +9,17 @@ class MenuItemAdmin(admin.ModelAdmin):
     to manage menu items for creation and updating.
     """
     model = MenuItem
-    list_display = [field.name for field in MenuItem._meta.get_fields()]
+    list_display = [
+        field.name for field in MenuItem._meta.get_fields()
+        if field.name != "allergens"
+    ]
     search_fields = [field.name for field in MenuItem._meta.get_fields()]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    MenuItemAdmin provides staff and site owner with ability
+    to manage menu items for creation and updating.
+    """
+    model = Category
