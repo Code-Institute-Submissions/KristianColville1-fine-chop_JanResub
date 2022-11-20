ec47e275f24f12de503bd9caac4c491426fcd7a5
KristianColville1 <kristiancolville1@gmail.com>
2022-11-20 15:04:54 +0000

    Added for loop to iterate and check url to highlight menu option

---
+++ menu/static/menu/js/food-menu.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    /**
+     * Secondary menu - food menu
+     * adds an active class to the food menu link with the correct url
+     * helps the user identify the page they are on and what menu they are
+     * currently viewing
+     */
+
+    const foodMenus = [
+        "starters",
+        "mains",
+        "noodles",
+        "rices",
+        "wok",
+        "curries",
+        "soups",
+        "vegan",
+        "salads",
+        "kids_menu",
+        "drinks",
+    ];
+    let stringURL = location.href;
+    let classToCall = "";
+    for (let i = 0; i < foodMenus.length; i++) {
+        if (stringURL.includes(foodMenus[i])) {
+            classToCall = foodMenus[i];
+        }
+    }
+    $(`.${classToCall}`).addClass("food-menu-active");
+});
