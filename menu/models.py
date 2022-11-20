6fa97add6358e3ae8b5fa5013d70fff5268955ab
KristianColville1 <kristiancolville1@gmail.com>
2022-11-20 16:33:06 +0000

    Set up menu items and begin creating fixtures

--- menu/models.py
+++ menu/models.py
@@ -1,20 +1,24 @@
 from django.db import models
 
-CATEGORY_CHOICES = (
-    ('A', 'Starters'),
-    ('B', 'Mains'),
-    ('C', 'Noodles'),
-    ('D', 'Rices'),
-    ('E', 'Wok'),
-    ('F', 'Curries'),
-    ('G', 'Soups'),
-    ('H', 'Vegan'),
-    ('I', 'Salads'),
-    ('J', 'Kids Menu'),
-    ('K', 'Drinks'),
-)
-
 STATUS_CHOICES = ((0, "Not Serving"), (1, "Serving"))
+
+
+class Category(models.Model):
+    """
+    Category model for food menu items
+    """
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
 
 
 class FoodItem(models.Model):
@@ -23,6 +27,7 @@ class FoodItem(models.Model):
     name = models.CharField(max_length=250, unique=True)
     description = models.TextField()
     image = models.ImageField(null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
 
     class Meta:
         verbose_name_plural = 'Food Items'
@@ -40,10 +45,22 @@ class FoodItemProductDetails(models.Model):
 
 class MenuItem(FoodItem, FoodItemProductDetails):
     """Menu item class mixin"""
-    category = models.CharField(choices=CATEGORY_CHOICES, max_length=1)
+    category = models.ForeignKey('Category',
+                                 null=True,
+                                 blank=True,
+                                 on_delete=models.SET_NULL)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS_CHOICES, default=1)
+    portion_sizes = models.BooleanField(default=False, null=True, blank=True)
+    rating = models.DecimalField(max_digits=6,
+                                 decimal_places=2,
+                                 null=True,
+                                 blank=True)
+    rating = models.DecimalField(max_digits=6,
+                                 decimal_places=2,
+                                 null=True,
+                                 blank=True)
 
     def __str__(self):
         """String representation"""
