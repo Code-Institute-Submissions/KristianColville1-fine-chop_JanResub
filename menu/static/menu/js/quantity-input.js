ba779f0e83b377fb0d2e21b2e28e770676052b60
KristianColville1 <kristiancolville1@gmail.com>
2022-11-25 11:39:00 +0000

    Added quantity inputs for +/- behaviour

---
+++ menu/static/menu/js/quantity-input.js
@@ -0,0 +1,41 @@
+// Disable +/- buttons outside 1-20 range
+function handleInputs(foodID) {
+    var currentValue = parseInt($(`#id_qty_${foodID}`).val());
+    var minusDisabled = currentValue < 2;
+    var plusDisabled = currentValue > 19;
+    $(`#decrement-qty_${foodID}`).prop("disabled", minusDisabled);
+    $(`#increment-qty_${foodID}`).prop("disabled", plusDisabled);
+}
+
+// Ensure proper enabling/disabling of all inputs on page load
+var allQtyInputs = $(".qty_input");
+for (var i = 0; i < allQtyInputs.length; i++) {
+    var foodID = $(allQtyInputs[i]).data("item_id");
+    handleInputs(foodID);
+}
+
+// Check enable/disable every time the input is changed
+$(".qty_input").change(function () {
+    var foodID = $(this).data("item_id");
+    handleInputs(foodID);
+});
+
+// Increment quantity
+$(".increment-qty").click(function (e) {
+    e.preventDefault();
+    var closestInput = $(this).closest(".input-group").find(".qty_input")[0];
+    var currentValue = parseInt($(closestInput).val());
+    $(closestInput).val(currentValue + 1);
+    var foodID = $(this).data("item_id");
+    handleInputs(foodID);
+});
+
+// Decrement quantity
+$(".decrement-qty").click(function (e) {
+    e.preventDefault();
+    var closestInput = $(this).closest(".input-group").find(".qty_input")[0];
+    var currentValue = parseInt($(closestInput).val());
+    $(closestInput).val(currentValue - 1);
+    var foodID = $(this).data("item_id");
+    handleInputs(foodID);
+});
