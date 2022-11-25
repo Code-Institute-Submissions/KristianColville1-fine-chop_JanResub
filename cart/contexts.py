7e55d60688dc907e4d9045ceb4bcbfb6d6f064d4
KristianColville1 <kristiancolville1@gmail.com>
2022-11-25 10:31:40 +0000

    Added menu item sizes logic for portion sizes of foods

--- cart/contexts.py
+++ cart/contexts.py
@@ -15,27 +15,43 @@ def cart_contents(request):
     delivery_cost = 0
     can_deliver = True
     cart = request.session.get('cart', {})
-    
-    for food_id, quantity in cart.items():
-        menu_item = get_object_or_404(MenuItem, pk=food_id)
-        total += quantity * menu_item.price
-        item_count += quantity
-        cart_items.append({
-            'food_id': food_id,
-            'quantity': quantity,
-            'menu_item': menu_item,
-        })
+
+    for food_id, food_amount in cart.items():
+        if isinstance(food_amount, int):
+            menu_item = get_object_or_404(MenuItem, pk=food_id)
+            total += food_amount * menu_item.price
+            item_count += food_amount
+            cart_items.append({
+                'food_id': food_id,
+                'quantity': food_amount,
+                'menu_item': menu_item,
+            })
+        else:
+            menu_item = get_object_or_404(MenuItem, pk=food_id)
+            for portion_size, quantity in food_amount[
+                    'menu_items_by_size'].items():
+                total += quantity * menu_item.price
+                item_count += quantity
+                cart_items.append({
+                    'food_id': food_id,
+                    'quantity': food_amount,
+                    'menu_item': menu_item,
+                    'portion_size': portion_size
+                })
+
     # calculates delivery cost based on kilometer
-    if user_distance < 3:
-        delivery_cost = settings.MINIMUM_DELIVERY_COST
-    else:
-        user_distance = user_distance - 3
-        delivery_total = user_distance / 1 * settings.DELIVERY_COST_PER_KM
-        delivery_total = Decimal(delivery_total + 3)
-        if delivery_total > 8.50:
-            can_deliver = False
+    # if the cart has items then add delivery cost
+    if len(cart_items) > 0:
+        if user_distance < 3:
+            delivery_cost = settings.MINIMUM_DELIVERY_COST
+        else:
+            user_distance = user_distance - 3
+            delivery_total = user_distance / 1 * settings.DELIVERY_COST_PER_KM
+            delivery_total = Decimal(delivery_total + 3)
             # if the total delivery cost is above this
             # then don't deliver as too far away
+            if delivery_total > 8.50:
+                can_deliver = False
 
     grand_total = delivery_cost + total
 
--- cart/views.py
+++ cart/views.py
@@ -15,12 +15,31 @@ def add_to_cart(request, menu_item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    portion_size = None
+
+    if 'portion_size' in request.POST:
+        portion_size = request.POST['portion_size']
     cart = request.session.get('cart', {})
 
-    if menu_item_id in list(cart.keys()):
-        cart[menu_item_id] += quantity
+    if portion_size:
+        if menu_item_id in list(cart.keys()):
+            if portion_size in cart[menu_item_id]['menu_items_by_size'].keys():
+                cart[menu_item_id]['menu_items_by_size'][
+                    portion_size] += quantity
+            else:
+                cart[menu_item_id]['menu_items_by_size'][
+                    portion_size] = quantity
+        else:
+            cart[menu_item_id] = {
+                'menu_items_by_size': {
+                    portion_size: quantity
+                }
+            }
     else:
-        cart[menu_item_id] = quantity
+        if menu_item_id in list(cart.keys()):
+            cart[menu_item_id] += quantity
+        else:
+            cart[menu_item_id] = quantity
 
     request.session['cart'] = cart
     print(request.session['cart'])
