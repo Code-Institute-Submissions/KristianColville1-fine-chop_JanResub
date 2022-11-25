4a3e3eb586b1b8a4e0d9016546fce16f5d282640
KristianColville1 <kristiancolville1@gmail.com>
2022-11-25 15:54:57 +0000

    Added update functionality for cart items and with portions

--- cart/static/cart/js/update-cart.js
+++ cart/static/cart/js/update-cart.js
@@ -1,16 +1,17 @@
 // Update quantity on click
-$(".update-link").click(function (e) {
-    var form = $(this).prev(".update-form");
+$("a.update-link").click(function (e) {
+    var form = $(".update-form");
     form.submit();
 });
 
 // Remove item from cart and reload on click
 $(".remove-item").click(function (e) {
-    var csrfToken = document.querySelector('meta[name="csrf-token"]').content;
-    var itemId = $(this).attr("id").split("remove_")[1];
-    var size = $(this).data("size");
-    var url = `/cart/remove/${itemId}`;
-    var data = { csrfmiddlewaretoken: csrfToken, size: size };
+    /* collects csrf_token */
+    let csrfToken = $("input[name=csrfmiddlewaretoken]").val();
+    var foodID = $(this).attr("id").split("remove_")[1];
+    var portion_size = $(this).data("portion_size");
+    var url = `/cart/remove/${foodID}/`;
+    var data = { csrfmiddlewaretoken: csrfToken, portion_size: portion_size };
 
     $.post(url, data).done(function () {
         location.reload();
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 def view_cart(request):
@@ -44,3 +44,57 @@ def add_to_cart(request, menu_item_id):
     request.session['cart'] = cart
     print(request.session['cart'])
     return redirect(redirect_url)
+
+
+def update_cart(request, food_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    portion_size = None
+    if 'portion_size' in request.POST:
+        portion_size = request.POST['portion_size']
+    cart = request.session.get('cart', {})
+
+    if portion_size == 'm':
+        print('I made it here')
+        if quantity > 0:
+            cart[food_id]['menu_items_by_size'][portion_size] = quantity
+
+        else:
+            del cart[food_id]['menu_items_by_size'][portion_size]
+            if not cart[food_id]['menu_items_by_size']:
+                cart.pop(food_id)
+
+    else:
+        if quantity > 0:
+            cart[food_id] = quantity
+        else:
+            cart.pop(food_id)
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
+
+
+def remove_from_cart(request, menu_item_id):
+    """
+    Removes quantity of the specified item from cart
+    """
+
+    try:
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['portion_size']
+        cart = request.session.get('cart', {})
+
+        if size:
+            del cart[menu_item_id]['menu_items_by_size'][size]
+            if not cart[menu_item_id]['menu_items_by_size']:
+                cart.pop(menu_item_id)
+        else:
+            cart.pop(menu_item_id)
+
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
