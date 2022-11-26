d9c33b60e0a744062244e54eceaaeb5e3564693d
KristianColville1 <kristiancolville1@gmail.com>
2022-11-26 17:11:03 +0000

    Added stripe element and tested stripe calls

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+import os
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from .forms import OrderForm
 
 
 def get_checkout(request):
-    """Get checkout page"""
-    return render(request, 'checkout/checkout.html')
+    """
+    Get checkout page
+    """
+    cart = request.session.get('cart', {})
+    if not cart:
+        messages.error(request, "There's nothing in the cart!")
+        return redirect(reverse('menu'))
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+        'stripe_public_key': os.environ.get("STRIPE_PUBLIC_KEY"),
+        'client_secret': 'test client secret',
+    }
+    return render(request, template, context)
