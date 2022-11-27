712927b0495d1c91d63f905b850da11e2113a417
KristianColville1 <kristiancolville1@gmail.com>
2022-11-27 09:04:21 +0000

    registered table and booking in admin

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from rangefilter.filters import DateRangeFilter
+from .models import Table, Booking
 
-# Register your models here.
+
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    """
+    Admin Model for Table arrangements
+    """
+    list_display = ('pk', 'table_number', 'seating_amount',
+                    'disabled_friendly')
+    list_filter = ('seating_amount', 'disabled_friendly')
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    """
+    Admin Model for Bookings
+    """
+    list_display = ('pk', 'customer', 'booked_table', 'guest_amount', 'date',
+                    'time')
+    search_fields = ['pk', 'customer__username']
+    list_filter = ('time', ('date', DateRangeFilter), 'booked_table')
