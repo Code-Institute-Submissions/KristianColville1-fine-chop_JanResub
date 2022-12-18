c4c0491adf529298b851d26cceeee29ff4f7cdd9
KristianColville1 <kristiancolville1@gmail.com>
2022-12-18 15:38:02 +0000

    Added add functionality to menu all page and categories

--- cart/urls.py
+++ cart/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.view_cart, name='view_cart'),
+    path('update/<food_id>/', views.update_cart, name='update_cart'),
     path('add/<menu_item_id>/', views.add_to_cart, name='add_to_cart'),
     path('remove/<menu_item_id>/',
          views.remove_from_cart,
          name='remove_from_cart'),
-    path('update/<food_id>/', views.update_cart, name='update_cart'),
 ]
