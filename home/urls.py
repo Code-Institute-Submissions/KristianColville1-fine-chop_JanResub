3fe487b3680ebb3c34c756c1ce554069439a030d
KristianColville1 <kristiancolville1@gmail.com>
2022-12-18 13:10:16 +0000

    Added views and templates for privacy policy and terms

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.get_home, name='home'),
     path('about/', views.get_about_page, name='about'),
+    path('privacy_policy/', views.privacy_policy, name='privacy_policy'),
+    path('terms_and_condition/',
+         views.terms_and_conditions,
+         name='terms_and_conditions')
 ]
--- home/views.py
+++ home/views.py
@@ -2,10 +2,28 @@ from django.shortcuts import render
 
 
 def get_home(request):
-    """Get home page"""
+    """
+    Get home page
+    """
     return render(request, 'home/index.html')
 
 
 def get_about_page(request):
-    """Get about page"""
+    """
+    Get about page
+    """
     return render(request, 'home/about.html')
+
+
+def privacy_policy(request):
+    """
+    Privacy Policy page
+    """
+    return render(request, 'home/privacy_policy.html')
+
+
+def terms_and_conditions(request):
+    """
+    Terms & conditions page
+    """
+    return render(request, 'home/terms_and_conditions.html')
