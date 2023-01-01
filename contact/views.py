5f33807a0044a5aa16fa4280bbc56e3af407418c
KristianColville1 <kristiancolville1@gmail.com>
2023-01-01 20:28:28 +0000

    Test contact emails are sending in live app on heroku

--- contact/views.py
+++ contact/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
 from .forms import ContactUsForm
 
 
@@ -6,11 +10,35 @@ def contact_us(request):
     """
     Contact us page
     """
-    # if request.method == 'POST':
-        
-    context = {
-        'contact_form': ContactUsForm
-    }
+
+    if request.method == 'POST':
+        form_data = {
+            'name': request.POST['name'],
+            'email': request.POST['email'],
+            'comment': request.POST['comment'],
+        }
+
+        contact_form = ContactUsForm(form_data)
+        if contact_form.is_valid():
+            form = contact_form
+            form.save()
+            pid = form.form.user_query_number
+            messages.info(request, ('Thanks for for your query'
+                                    'Please check your email.'
+                                    f'Your query number is {pid}'))
+            their_email = form.email
+            subject = render_to_string(
+                'contact/confirmation_emails/confirmation_email_subject.txt',
+                {'form': form})
+            body = render_to_string(
+                'contact/confirmation_emails/confirmation_email_body.txt',
+                {'form': form})
+            send_mail(subject, body, settings.DEFAULT_FROM_EMAIL,
+                      [their_email])
+        else:
+            messages.error(request, ('There was an error with your form. '
+                                     'Please double check your information.'))
+    context = {'contact_form': ContactUsForm}
     return render(request, 'contact/contact_us.html', context)
 
 
