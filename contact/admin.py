56a4fd44a5f8b7a89794980849c1dfa573101e5f
KristianColville1 <kristiancolville1@gmail.com>
2023-01-01 17:32:46 +0000

    Added user query model for contacting finechop and added admin model

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import UserQuery
 
-# Register your models here.
+
+@admin.register(UserQuery)
+class UserQueryAdmin(admin.ModelAdmin):
+    """
+    UserQueryAdmin provides staff and site owner with ability
+    to manage feedback and user queries.
+    """
+    model = UserQuery
+    list_display = [
+        field.name for field in UserQuery._meta.get_fields()
+    ]
+    search_fields = [field.name for field in UserQuery._meta.get_fields()]
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,34 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class UserQuery(models.Model):
+    """
+    UserQuery Model for users interaction and feedback
+    """
+    name = models.CharField(max_length=80)
+    email = models.CharField(max_length=320, unique=False)
+    comment = models.TextField()
+    user_query_number = models.CharField(max_length=254,
+                                         null=False,
+                                         blank=False,
+                                         default='')
+    is_open = models.BooleanField(default=True)
+
+    def _generate_user_query_number(self):
+        """
+        Private Method generates a random, unique user query number using UUID
+        """
+        return uuid.uuid4().hex.upper()
+
+    def save(self, *args, **kwargs):
+        """
+        Overrides the original save method to set the user query number
+        if it hasn't been set already.
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.user_query_number
