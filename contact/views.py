e616451d59ef6d7190061e95a7ff458045a4206b
KristianColville1 <kristiancolville1@gmail.com>
2023-01-02 17:04:55 +0000

    Added views and validator for file size

--- contact/views.py
+++ contact/views.py
@@ -3,7 +3,7 @@ from django.contrib import messages
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.conf import settings
-from .forms import ContactUsForm
+from .forms import ContactUsForm, JobApplicationForm
 
 
 def contact_us(request):
@@ -12,7 +12,6 @@ def contact_us(request):
     """
 
     if request.method == 'POST':
-        print('here')
         form_data = {
             'name': request.POST['name'],
             'email': request.POST['email'],
@@ -47,4 +46,36 @@ def careers(request):
     """
     Career page
     """
-    return render(request, 'contact/careers.html')
+
+    if request.POST:
+        form_data = {
+            'first_name': request.POST['first_name'],
+            'last_name': request.POST['last_name'],
+            'email': request.POST['email'],
+            'contact_number': request.POST['contact_number'],
+            'message': request.POST['message'],
+            'resume': request.FILES['resume'],
+        }
+        form = JobApplicationForm(request.POST, request.FILES)
+        if form.is_valid():
+            form = form.save()
+            pid = form.application_number
+            messages.info(request, ('Thanks for for your application '
+                                    'We will send you a confirmation email.'
+                                    f' Your application number is {pid}'))
+            their_email = form.email
+            subject = render_to_string(
+                'contact/confirmation_emails/confirmation_email_subject.txt',
+                {'form': form})
+            body = render_to_string(
+                'contact/confirmation_emails/confirmation_email_body.txt',
+                {'form': form})
+            send_mail(subject, body, settings.DEFAULT_FROM_EMAIL,
+                      [their_email])
+            return redirect('/')
+        else:
+            messages.error(request, ('There was an error with your form. '
+                                     'Please double check your information.'
+                                     'Please try again!'))
+    context = {'form': JobApplicationForm}
+    return render(request, 'contact/careers.html', context)
