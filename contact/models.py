9058237a08cc4b9850ab7725b3cd354cd3edffdf
KristianColville1 <kristiancolville1@gmail.com>
2023-01-02 17:03:54 +0000

    Added job application models and response models

--- contact/models.py
+++ contact/models.py
@@ -1,8 +1,8 @@
 import uuid
 from django.db import models
 from django.core.mail import send_mail
-from django.template.loader import render_to_string
 from django.conf import settings
+from .file_size import file_size
 
 
 class UserQuery(models.Model):
@@ -44,11 +44,9 @@ class QueryResponse(models.Model):
     """
     Query Response class allows super users to reply to user query.
     """
-    ticket = models.OneToOneField(
-        UserQuery,
-        on_delete=models.CASCADE,
-        related_name='user_query_response'
-    )
+    ticket = models.OneToOneField(UserQuery,
+                                  on_delete=models.CASCADE,
+                                  related_name='user_query_response')
     reply = models.TextField()
     resolved = models.BooleanField(default=False)
 
@@ -65,9 +63,85 @@ class QueryResponse(models.Model):
     def save(self, *args, **kwargs):
         """
         Overrides the original save method to send a reply to the user
-        if it hasn't been set already and sent.
+        if it hasn't been set already and sent. Only sends the reply if
+        the superuser has resolved the issue.
         """
-        if self.resolved is False:
+        if self.resolved is True:
+            self._send_reply_to_email()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.reply
+
+
+class JobApplication(models.Model):
+    """
+    JobApplication Model for users to submit applications
+    to work at FineChop.
+    """
+    first_name = models.CharField(max_length=80)
+    last_name = models.CharField(max_length=80)
+    email = models.CharField(max_length=320, unique=False)
+    contact_number = models.IntegerField(unique=False)
+    message = models.TextField()
+    resume = models.FileField(upload_to='static/resumes/',
+                              validators=[file_size])
+    application_number = models.CharField(max_length=100,
+                                          null=False,
+                                          blank=False,
+                                          default='')
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'Job Applications'
+        ordering = ['created_on']
+
+    def _generate_application_number(self):
+        """
+        Private Method generates a random, unique application number using UUID
+        """
+        return uuid.uuid4().hex.upper()
+
+    def save(self, *args, **kwargs):
+        """
+        Overrides the original save method to set the user query number
+        if it hasn't been set already.
+        """
+        if not self.application_number:
+            self.application_number = self._generate_application_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.application_number
+
+
+class JobApplicationResponse(models.Model):
+    """
+    Query Response class allows super users to reply to user query.
+    """
+    application = models.OneToOneField(JobApplication,
+                                       on_delete=models.CASCADE,
+                                       related_name='job_application')
+    reply = models.TextField()
+    is_successful = models.BooleanField(default=False)
+
+    def _send_reply_to_email(self):
+        """
+        Private method sends users a reply to their email
+        """
+        their_email = self.application.email
+        subject = 'Application: ' + self.application.application_number
+        body = self.reply
+
+        send_mail(subject, body, settings.DEFAULT_FROM_EMAIL, [their_email])
+
+    def save(self, *args, **kwargs):
+        """
+        Overrides the original save method to send a reply to the user
+        if it hasn't been set already and sent. Only sends the reply if
+        the superuser has deemed the application successful.
+        """
+        if self.is_successful is True:
             self._send_reply_to_email()
         super().save(*args, **kwargs)
 
