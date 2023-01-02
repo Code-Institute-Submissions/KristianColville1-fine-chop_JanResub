2ed88310c1daf877337ef137ae11853f0b6ce6b2
KristianColville1 <kristiancolville1@gmail.com>
2023-01-02 17:04:31 +0000

    Added admin views for job application and forms

--- contact/admin.py
+++ contact/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import UserQuery, QueryResponse
+from .models import (UserQuery, QueryResponse, JobApplication,
+                     JobApplicationResponse)
 
 
 class QueryResponseAdmin(admin.TabularInline):
@@ -19,8 +20,32 @@ class UserQueryAdmin(admin.ModelAdmin):
     to manage feedback and user queries.
     """
     model = UserQuery
-    list_display = [
-        field.name for field in UserQuery._meta.get_fields()
-    ]
+    list_display = [field.name for field in UserQuery._meta.get_fields()]
     search_fields = [field.name for field in UserQuery._meta.get_fields()]
     inlines = [QueryResponseAdmin]
+
+
+class JobApplicationResponseAdmin(admin.TabularInline):
+    """
+    UserQueryAdmin provides staff and site owner with ability
+    to manage feedback and user queries.
+    """
+    model = JobApplicationResponse
+    list_display = [
+        field.name for field in JobApplicationResponse._meta.get_fields()
+    ]
+    search_fields = [
+        field.name for field in JobApplicationResponse._meta.get_fields()
+    ]
+
+
+@admin.register(JobApplication)
+class JobApplicationAdmin(admin.ModelAdmin):
+    """
+    UserQueryAdmin provides staff and site owner with ability
+    to manage feedback and user queries.
+    """
+    model = JobApplication
+    list_display = [field.name for field in JobApplication._meta.get_fields()]
+    search_fields = [field.name for field in JobApplication._meta.get_fields()]
+    inlines = [JobApplicationResponseAdmin]
--- contact/forms.py
+++ contact/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import UserQuery
+from .models import UserQuery, JobApplication
 
 
 class ContactUsForm(forms.ModelForm):
@@ -28,6 +28,46 @@ class ContactUsForm(forms.ModelForm):
             'Enter your query/feedback here and we will be in touch..',
         }
         self.fields['name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} '
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].label = False
+
+
+class JobApplicationForm(forms.ModelForm):
+    """
+    Job Application Us Form for submitting user queries to finechop
+    """
+
+    class Meta:
+        model = JobApplication
+        fields = (
+            'first_name',
+            'last_name',
+            'email',
+            'contact_number',
+            'message',
+            'resume',
+        )
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'email': 'Email',
+            'contact_number': 'Contact Number',
+            'message': 'Message',
+            'resume': 'Resume',
+        }
+        self.fields['first_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
             if self.fields[field].required:
                 placeholder = f'{placeholders[field]} '
